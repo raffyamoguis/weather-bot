2a4b6387ec4ff36ac7a756b1c3a91892f1de8a24
unknown <raffyamoguis@gmail.com>
2022-11-18 18:23:32 +0800

    added something

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -4,7 +4,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => {
   console.log('Received /start command');
   try {
-    return ctx.reply('Hi');
+    return ctx.reply('Hi motherfucker!');
   } catch (e) {
     console.error('error in start action:', e);
     return ctx.reply('Error occured');
